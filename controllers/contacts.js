5f30d7ca227d2af534c73edd3e01302a70d8a0cb
shauniebyrne <byr21002@byui.edu>
2023-04-26 16:21:26 -0700

    closer

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,7 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
+//Read (GET) all contacts in database
 const getAllData = async (req, res, next) => {
     const result = await mongodb
         .getDb()
@@ -13,6 +14,7 @@ const getAllData = async (req, res, next) => {
     });
 };
 
+//Read (GET) one contact (based off Id) in database
 const getSingleData = async (req, res, next) => {
     const UserId = new ObjectId(req.params.id);
     const result = await mongodb
@@ -26,4 +28,26 @@ const getSingleData = async (req, res, next) => {
     });
 };
 
-module.exports = { getAllData, getSingleData};
+//Create (POST) a new contact
+const createContact = async (req, res, next) => {
+    const result = await mongodb
+        .getDb()
+        .db('cse341')
+        .collection('contacts')
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
+};
+
+//Update (PUT) an old contact
+const updateContact = async (req, res, next) => {
+
+};
+
+//Delete (DELETE) a contact
+const deleteContact = async (req, res, next) => {
+
+};
+
+module.exports = { getAllData, getSingleData, createContact, updateContact, deleteContact};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 const contactsController = require('../controllers/contacts');
 
+//Read (Get) records from database
 router.get('/', contactsController.getAllData);
 router.get('/:id', contactsController.getSingleData);
+
+//Create (POST) a new contact
+router.post('/', contactsController.createContact);
+
+//Update (PUT) an old contact
+router.put('/:id', contactsController.updateContact);
+
+//Delete (DELETE) a contact
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
