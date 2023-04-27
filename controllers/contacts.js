afd30f07ac75cfeab065934b042973cba1f912ff
shauniebyrne <byr21002@byui.edu>
2023-04-27 00:46:50 -0700

    almost perfect

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -30,24 +30,65 @@ const getSingleData = async (req, res, next) => {
 
 //Create (POST) a new contact
 const createContact = async (req, res, next) => {
-    const result = await mongodb
+    //New Contact Info
+    const newContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favColor: req.body.favColor,
+        birthday: req.body.birthday
+    };
+    //Connect to database
+    const resultBack = await mongodb
         .getDb()
         .db('cse341')
         .collection('contacts')
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
+        .insertOne(newContact);
+    if(resultBack.acknowledged) {
+        res.status(201).json(resultBack);
+    } else {
+        res.status(500).json(resultBack.error || 'Sorry. Contact was not created.');
+    }
 };
 
 //Update (PUT) an old contact
 const updateContact = async (req, res, next) => {
-
+    const UserId = new ObjectId(req.params.id);
+    const updatedInfo = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favColor: req.body.favColor,
+        birthday: req.body.birthday
+    };
+    const resultBack = await mongodb
+        .getDb()
+        .db('cse341')
+        .collection('contacts')
+        .replaceOne({ _id: UserId}, updatedInfo);
+    console.log(resultBack.nModified + 'document(s) were updated');
+    if(resultBack.nModified > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(resultBack.error || 'Sorry. New information could not be updated.');
+    }    
 };
 
 //Delete (DELETE) a contact
 const deleteContact = async (req, res, next) => {
-
+    const UserId = new ObjectId(req.params.id);
+    const resultBack = await mongodb
+        .getDb()
+        .db('cse341')
+        .collection('contacts')
+        .deleteOne({ _id: UserId}, true);
+    console.log(resultBack.deletedCount + 'document(s) were deleted.');
+    if(resultBack.deleteCount > 0) {
+        res.status(200).send();
+    } else {
+        res.status(500).json(resultBack.error || 'Sorry. Information was not deleted.');
+    }
+    
 };
 
 module.exports = { getAllData, getSingleData, createContact, updateContact, deleteContact};
